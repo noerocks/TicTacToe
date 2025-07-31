9bcafa2f03aa3d7977496fa04bd9f3463ec09c7a
Noe Villamor <noejayar@gmail.com>
2025-07-31 19:42:32 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="h-screen flex flex-col justify-center items-center gap-2 bg-gray-800 text-white">
+        <h1 className="text-6xl font-bold">Tic Tac Toe</h1>
+        <p className="text-2xl">My first React project⚛️</p>
+      </div>
+    );
+  }
+}
+
+export default App;
