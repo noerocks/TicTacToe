72c392cf61e8e5140b1a782d46b2047c20c0b94b
Noe Villamor <noejayar@gmail.com>
2025-07-31 20:13:11 +0800

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 class App extends React.Component {
   render() {
+    console.log("test");
     return (
       <div className="h-screen flex flex-col justify-center items-center gap-2 bg-gray-800 text-white">
         <h1 className="text-6xl font-bold">Tic Tac Toe</h1>
